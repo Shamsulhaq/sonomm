3ab2eb9a7eec3d037d5665499d200a95f2983165
Shamsulhaq <bmshamsulhaq65@gmail.com>
2018-09-16 17:35:25 +0600

    Add Discunt Genaretor

--- ecommarce/product_model.py
+++ ecommarce/product_model.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from sonomm.utils import unique_slug_generator
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     image = models.ImageField(upload_to='category/main', blank=True)
@@ -9,19 +10,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class SubCategory(models.Model):
-    mainCat = models.ForeignKey(Category,on_delete=models.CASCADE);
-    name =models.CharField(max_length=100,unique=True)
+    mainCat = models.ForeignKey(Category, on_delete=models.CASCADE);
+    name = models.CharField(max_length=100, unique=True)
     image = models.ImageField(upload_to='category/sub', blank=True)
 
     def __str__(self):
         return self.name
 
+
 # Create Product Model here.
 class ProductBasic(models.Model):
     name = models.CharField(max_length=200, help_text='Enter your Product Title')
     category = models.ManyToManyField(SubCategory)
-    regular_price = models.DecimalField(max_digits=9, decimal_places=2, default=100.00, blank=True, null= True)
+    regular_price = models.DecimalField(max_digits=9, decimal_places=2, default=100.00, blank=True, null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=100.00)
     quantity = models.PositiveIntegerField(default=1)
     unit = models.PositiveIntegerField(default=1)
@@ -41,9 +44,17 @@ class ProductBasic(models.Model):
     def __str__(self):
         return self.name
 
+    def get_discunt(self):
+        dis = ((self.regular_price - self.price) / self.regular_price) * 100
+        discubt = int(dis)
+        return discubt
+
+
+
     @property
     def title(self):
         return self.name
+
 
 class ProductGallery(models.Model):
     product = models.OneToOneField(ProductBasic, on_delete=models.CASCADE)
@@ -56,12 +67,9 @@ class ProductGallery(models.Model):
         return self.product.title
 
 
-
 def pd_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
 
 pre_save.connect(pd_pre_save_receiver, sender=ProductBasic)
-
-
